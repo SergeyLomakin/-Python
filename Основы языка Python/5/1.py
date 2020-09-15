9c8ef5f951b192a93ae0fdde113a1e961cea0ef9
SergeyLomakin <58804038+SergeyLomakin@users.noreply.github.com>
2020-09-15 15:27:40 +0500

    Задания урока 5

--- Основы языка Python/5/1.py
+++ Основы языка Python/5/1.py
@@ -6,7 +6,7 @@ def user_in ():
     # функция записывает вводимые пользователем строки в текстовый файл.
     line = input('Введите строку: ')
     while line != '':
-        with open("text.txt", "a") as txt:
+        with open("text_1.txt", "a") as txt:
             txt.write(line + '\n')
         return user_in()
 
