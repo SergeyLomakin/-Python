42ee4255a44b003ae2e3a3edf490ed878a96f892
SergeyLomakin <58804038+SergeyLomakin@users.noreply.github.com>
2020-09-17 17:14:50 +0500

    Задания урока 6

--- Основы языка Python/6/4.py
+++ Основы языка Python/6/4.py
@@ -11,4 +11,84 @@
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат."""
 
+import random
 
+
+class Car:
+    def __init__(self, speed, color, name, is_police=False):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+        print(f'Автомобиль марки: {self.name}, передвигается со скоростью: {self.speed} км/ч, '
+              f'цвет автомобиля: {self.color}.')
+
+    def go(self):
+        return 'Машина поехала'
+
+    def stop(self):
+        return 'Машина остановилась'
+
+    def turn(self):
+        self = ['Юг', 'Юго-Восток', 'Восток', 'Северо-Восток',
+                'Север', 'Северо-Запад', 'Запад', 'Юго-Запад']
+        return f'Автомобиль двигается на: {self[random.randint(0, 7)]}'
+
+    def show_speed(self):
+        return f'Скорость автомобиля составляет: {self.speed} км/ч'
+
+
+class TownCar(Car):
+    def show_speed(self):
+        if self.speed > 60:
+            return f'Автомобиль является городским. ' \
+                   f'Превышение скорости на {self.speed - 60} км/ч. ' \
+                   f'Допустимая скорость составляет 60 км/ч'
+
+
+class SportCar(Car):
+    pass
+
+
+class WorkCar(Car):
+    def show_speed(self):
+        if self.speed > 40:
+            return f'Автомобиль является техническим. ' \
+                   f'Превышение скорости на {self.speed - 40} км/ч. ' \
+                   f'Допустимая скорость составляет 40 км/ч'
+
+
+class PoliceCar(Car):
+    def police(self):
+        if self.is_police is not False:
+            return f'Автомобиль является полицейским.'
+
+
+auto_1 = TownCar(120, 'черный', 'BMW')
+print(auto_1.show_speed())
+print(auto_1.go())
+print(auto_1.stop())
+print(auto_1.turn())
+print()
+
+auto_2 = PoliceCar(160, 'белый', 'Audi', True)
+print(auto_2.show_speed())
+print(auto_2.go())
+print(auto_2.stop())
+print(auto_2.turn())
+print(auto_2.police())
+print()
+
+auto_3 = WorkCar(60, 'жёлтый', 'Caterpillar')
+print(auto_3.show_speed())
+print(auto_3.go())
+print(auto_3.stop())
+print(auto_3.turn())
+print()
+
+auto_4 = SportCar(250, 'красный', 'Ferrari')
+print(auto_4.show_speed())
+print(auto_4.go())
+print(auto_4.stop())
+print(auto_4.turn())
+print()
