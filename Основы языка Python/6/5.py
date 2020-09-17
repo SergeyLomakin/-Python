7d418cde39c786be0e5ae8453caa529506405d1f
SergeyLomakin <58804038+SergeyLomakin@users.noreply.github.com>
2020-09-17 21:31:11 +0500

    Задания урока 6

--- Основы языка Python/6/5.py
+++ Основы языка Python/6/5.py
@@ -8,3 +8,37 @@
 что выведет описанный метод для каждого экземпляра."""
 
 
+class Stationery:
+  def __init__(self, title=''):
+    self.title = title
+    
+  def draw(self):
+    return f'запуск отрисовки {self.title}'
+
+
+class Pen(Stationery):
+  def draw(self):
+    self.title = 'ручкой'
+    return f'запуск отрисовки {self.title}'
+
+
+class Pencil(Stationery):
+  def draw(self):
+    self.title = 'карандашом'
+    return f'запуск отрисовки {self.title}'
+
+
+class Handle(Stationery):
+  def draw(self):
+    self.title = 'маркером'
+    return f'запуск отрисовки {self.title}'
+    
+
+a = Stationery()
+print(a.draw())
+b = Pen()
+print(b.draw())
+c = Pencil()
+print(c.draw())
+d = Handle()
+print(d.draw())
